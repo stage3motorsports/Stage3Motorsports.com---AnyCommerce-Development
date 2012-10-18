96726b50cdac3c8ce496489fd3b5a8fc8780ead1
michael <michaelc@zoovy.com>
2012-10-18 12:05:04 -0700

    Tour, Financing load using page handler, with Inits

--- _stage3_init.js
+++ _stage3_init.js
@@ -20,7 +20,8 @@ app.vars.extensions = [
 
 
 app.vars.catTemplates = {
-	".975silverstang.stage_3_shop_tour":"categoryTemplateTour"
+	".975silverstang.stage_3_shop_tour":"categoryTemplateTour",
+	".financing":"categoryTemplateTour"
 	};
 
 
@@ -135,6 +136,7 @@ app.u.appInitComplete = function()	{
 	
 	app.ext.myRIA.template.homepageTemplate.onInits.push(function(P) {app.u.showTierOne();})
 	app.ext.myRIA.template.categoryTemplate.onInits.push(function(P) {app.u.hideTierOne();})
+	app.ext.myRIA.template.categoryTemplateTour.onInits.push(function(P) {app.u.hideTierOne();})
 	app.ext.myRIA.template.productTemplate.onInits.push(function(P) {app.u.hideTierOne();})
 	app.ext.myRIA.template.companyTemplate.onInits.push(function(P) {app.u.hideTierOne();})
 	app.ext.myRIA.template.customerTemplate.onInits.push(function(P) {app.u.hideTierOne();})
